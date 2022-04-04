80b426eecd2d9baf3828ea6d0aeeed7b3d8c331e
MilonMridha <milonmridha570@gmail.com>
2022-04-03 20:50:35 -0700

    adde-optional-image

--- src/Components/CustomerReviews/CustomerReviews.js
+++ src/Components/CustomerReviews/CustomerReviews.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import './CustomerRewiew.css'
 
 
 const CustomerReviews = (props) => {
     const { review } = props;
-    const { name, reviews, ratting } = review;
+    const { name, reviews, ratting,img } = review;
 
     return (
         <div>
             <Card style={{ width: '18rem' }} className="shadow rounded">
                 <Card.Body className='bg-light'>
+                    <img className='review-img' src={img} alt="" />
                     <Card.Title>Name: {name}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted"> Ratting : {ratting}</Card.Subtitle>
                     <Card.Text> <h6>Reviews :</h6>
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-
+import './Review.css'
 const Review = (props) => {
     const {review} = props;
-    const {name, reviews, ratting} = review;
+    const {name, reviews, ratting, img} = review;
     return (
         <div className='container'>
              <Card style={{ width: '18rem' }} className="shadow rounded">
+                 
                 <Card.Body className='bg-light'>
+                <img className='review-img' src={img} alt="" />
                     <Card.Title>Name: {name}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted"> Ratting : {ratting}</Card.Subtitle>
                     <Card.Text> <h6>Reviews :</h6>
